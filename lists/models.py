f1ef48fddd2ed37e98e9c92d40ca11856c1dc0ed
Ro-Goncalves <53356279+Ro-Goncalves@users.noreply.github.com>
2020-02-17 10:46:50 -0300

    new URL + view for adding to existing lists. FT passes :-)

--- lists/models.py
+++ lists/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class List(models.Model):
+    #id = models.IntegerField(primary_key=True)
     pass
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None)
+    list = models.ForeignKey(List
+                             ,default=None
+                             ,null=True
+                             ,blank=True)
     
--- superlist/urls.py
+++ superlist/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     url(r'^lists/new$'
         ,views.new_list
         ,name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$'
+    url(r'^lists/(\d+)/$'
         ,views.view_list
         ,name='view_list'),
+    url(r'^lists/(\d+)/add_item$'
+        ,views.add_item,
+        name='add_item'),
 ]
